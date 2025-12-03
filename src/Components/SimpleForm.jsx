077a1c9803388e7989f0b0f658184df338ed45d0
nothing36 <prodipsarker953@gmail.com>
2025-12-03 11:05:37 +0600

    get value by target

--- src/Components/SimpleForm.jsx
+++ src/Components/SimpleForm.jsx
@@ -3,13 +3,15 @@ import React from "react";
 const SimpleForm = () => {
     const handleSubmit=(event)=>{
         event.preventDefault();
-        console.log("Form submitted",event);
+        console.log("Name: ",event.target.name.value);
+        console.log("Email: ",event.target.email.value);
 
     }
   return (
     <div className="flex justify-center w-11/12 mx-auto mt-10">
-      <form className="text-center" onSubmit={()=>handleSubmit(event)}>
+      <form className="text-center" onSubmit={handleSubmit}>
         <input name="name" type="text" placeholder="Type here" className="input" />
+        <input type="email" name="email" id="" placeholder="email" className="input" />
         <br />
         <input className="bg-gray-400 mt-3 p-3 rounded-lg" type="submit" value="Submit" />
       </form>
