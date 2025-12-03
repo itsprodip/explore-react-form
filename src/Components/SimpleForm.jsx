078e33fb7dada7a90c75fb6873f60260cdf81d45
nothing36 <prodipsarker953@gmail.com>
2025-12-03 10:44:08 +0600

    form created

--- src/Components/SimpleForm.jsx
+++ src/Components/SimpleForm.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 const SimpleForm = () => {
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        console.log("Form submitted",event);
+
+    }
   return (
     <div className="flex justify-center w-11/12 mx-auto mt-10">
-      <form className="text-center">
-        <input type="text" placeholder="Type here" className="input" />
+      <form className="text-center" onSubmit={()=>handleSubmit(event)}>
+        <input name="name" type="text" placeholder="Type here" className="input" />
         <br />
-        <input className="" type="submit" value="Submit" />
+        <input className="bg-gray-400 mt-3 p-3 rounded-lg" type="submit" value="Submit" />
       </form>
     </div>
   );
