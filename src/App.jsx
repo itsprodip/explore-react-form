560355576766a17c93399f15822d22e46d24bc6e
nothing36 <prodipsarker953@gmail.com>
2025-12-05 22:16:23 +0600

    product cards added

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import SimpleForm from "./Components/SimpleForm";
 import FormAction from "./Components/FormAction";
 import ControlledFilled from "./Components/ControlledFilled";
 import UncontrolledField from "./Components/UncontrolledField";
+import ProductManagement from "./Components/ProductManagement/ProductManagement";
 
 const App = () => {
   return (
     <div>
+      <ProductManagement/>
       <UncontrolledField/>
       <SimpleForm />
       <FormAction />
---
+++ src/Components/ProductManagement/ProductForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ProductForm = () => {
+    const handleProductSubmit= event=>{
+        event.preventDefault();
+        const name=event.target.name.value;
+        const price=event.target.price.value;
+        const quantity=event.target.quantity.value;
+        // console.log(name,price,quantity);
+        const newProduct={
+            name,price,quantity
+        }
+        console.log(newProduct);
+    }
+    return (
+        <div className='text-center mt-10'>
+   <div>
+             <h2>Product Form</h2>
+            <form onSubmit={handleProductSubmit}>
+            <input type="text" name="name" placeholder='Product Name' id="" className='input'/>
+            <br />
+            <input type="text" name="price" placeholder='Product Price' id=""  className='input'/>
+            <br />
+            <input type="text" name="quantity" placeholder='Product Quantity' id="" className='input'/>
+            <br />
+            <input type="submit" value="Submit" />
+            </form>
+   </div>
+        </div>
+    );
+};
+
+export default ProductForm;
