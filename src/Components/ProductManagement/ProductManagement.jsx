2f2f657da7d7092c1e929365e7f6033df38f2431
nothing36 <prodipsarker953@gmail.com>
2025-12-06 21:55:52 +0600

    products updated

--- src/Components/ProductManagement/ProductManagement.jsx
+++ src/Components/ProductManagement/ProductManagement.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductForm from './ProductForm';
 import ProductTable from './ProductTable';
 
 const ProductManagement = () => {
+    const [products,setProducts]=useState([]);
+
+    const handleAddProduct = newProduct=>
+    {
+        const newProducts=[...products,newProduct];
+        setProducts(newProducts);
+    }
     return (
         <div>
-                <ProductForm/>
+                <ProductForm handleAddProduct={handleAddProduct}/>
                 <ProductTable/>
         </div>
     );
